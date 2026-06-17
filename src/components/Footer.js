a07a84b6f807ce8dbc59b78d0430bd8c8f6cac45
Fabien Royer <royer.tech@outlook.com>
2026-06-17 23:53:15 +0200

    use state cart

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer() {
 	return (
 		<footer className='lmj-footer'>
 			<div className='lmj-footer-elem'>
-				Pour les passionné·e·s de plantes 🌿🌱🌵
+				Pour les passionnés de plantes 🌿🌱🌵
 			</div>
 			<div className='lmj-footer-elem'>Laissez-nous votre mail :</div>
 			<input
