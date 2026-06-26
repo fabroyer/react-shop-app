d51ad530bc7dfbee83572fe0e3a79703f9af6e27
Fabien Royer <royer.tech@outlook.com>
2026-06-26 22:07:00 +0200

    categories.js and plantitem.js

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -5,9 +5,10 @@ function handleClick(plantName) {
 	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
 }
 
-function PlantItem({ id, cover, name, water, light }) {
+function PlantItem({ cover, name, water, light, price }) {
 	return (
 		<li className='lmj-plant-item' onClick={() => handleClick(name)}>
+			<span className='lmj-plant-item-price'>{price}€</span>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
